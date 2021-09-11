dfe8ddd96727866a8268a45be70c5e982965ee87
Artimirche6 <artimirche6@gmail.com>
2021-09-12 00:46:27 +0530

    [arti] Added: Flip to display HEAD or TAIL

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,3 +1,12 @@
 #! /bin/bash -x
 
 echo "This problem displays winning percentage of Head or Tail Combination in a Singlet, Doublet and Triplet"
+
+flip=$(($RANDOM%2))
+
+if [ $flip -eq 1 ]
+then
+	echo "HEAD"
+else
+	echo "TAIL"
+fi
