4a75a6b6d3b94c6ee8891c81e1cb1bb55d9b700d
Artimirche6 <artimirche6@gmail.com>
2021-09-12 01:07:04 +0530

    [arti] Added: Doublet Combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,36 +1,50 @@
-#!/bin/bash -x
+#!/bin/bash 
 
-declare -A singlet
+declare -A doublet
 declare -A percent
 
-read -p "Enter how many times you want to flip coin: " num
-
-head=0
-tail=0
+num=21
+TT=0
+TH=0
+HT=0
+HH=0
 
 for((i=1; i<=$num; i++))
 do
-	flip=$(($RANDOM%2))
-	
-	if [ $flip -eq 1 ]
+	flip1=$(($RANDOM%2))
+	flip2=$(($RANDOM%2))
+
+	if [ $flip1 -eq 0 ] && [ $flip2 -eq 0 ] 
 	then
-		echo "HEAD"
-		head=$(($head+1))
+		echo "TT"
+		TT=$(($TT+1))
+	elif  [ $flip1 -eq 0 ] && [ $flip2 -eq 1 ]
+	then
+   		echo "TH"
+   		TH=$(($TH+1))
+	elif [ $flip1 -eq 1 ] && [ $flip2 -eq 0 ]
+	then
+   		echo "HT"
+   		HT=$(($HT+1))
+	elif  [ $flip1 -eq 1 ] && [ $flip2 -eq 1 ]
+	then
+   		echo "HH"
+   	HH=$(($HH+1))
 	else
-		echo "TAIL"
-		tail=$(($tail+1))
-	fi
-done
+		echo "invalid"
+	fi 
+done 
 
-singlet[H]=$head
-singlet[T]=$tail
+doublet[TT]=$TT 
+doublet[TH]=$TH
+doublet[HT]=$HT
+doublet[HH]=$HH
 
-echo "Number of head= $head"
-echo "Number of tail= $tail"
+echo "Doublet Head and Tail resp : doublet: ${doublet[*]}"
 
-echo "Singlet of Head and Tail resp : ${singlet[*]}"
+percent[TT]=$(($TT*100/21))
+percent[TH]=$(($TH*100/21))
+percent[HT]=$(($HT*100/21))
+percent[HH]=$(($HH*100/21)) 
 
-percent[H]=$(($head*100/21))
-percent[T]=$(($tail*100/21))
-
-echo "Percentage of Head and Tail resp : ${percent[*]}"
+echo "Percentage Head and Tail resp : percent: ${percent[*]}"
